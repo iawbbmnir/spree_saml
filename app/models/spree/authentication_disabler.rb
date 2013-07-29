c29de4843bdf694b39e6015dad726272a0b200f2
Jerry Feng <zfeng@2keys.ca>
2013-07-29 11:52:11 -0400

    empty_table_fix

--- app/models/spree/authentication_disabler.rb
+++ app/models/spree/authentication_disabler.rb
@@ -2,5 +2,14 @@ class Spree::AuthenticationDisabler
   def self.login_disabled
     return false
   end
+
+  def self.active_saml
+    config = Spree::SamlConfig.where(:is_active => true).first
+    if config.nil?
+      return false
+    else
+      return true
+    end
+  end
 end
 
